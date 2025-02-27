682ab384492907685285b158a4a64af825da2a56
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-02-27 14:29:46 -0400

    Initial commit

---
+++ tomevault-api/src/main/java/com/cristianml/TomeVault/TomevaultApplication.java
@@ -0,0 +1,13 @@
+package com.cristianml.TomeVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomevaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomevaultApplication.class, args);
+	}
+
+}
