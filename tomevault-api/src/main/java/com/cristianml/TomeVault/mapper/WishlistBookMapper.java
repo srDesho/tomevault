c08789a607f106ff74ab837916a34c3ca9b1fa5f
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-04-15 00:44:19 -0400

    Creating Mappers, repositories

---
+++ tomevault-api/src/main/java/com/cristianml/TomeVault/mapper/WishlistBookMapper.java
@@ -0,0 +1,27 @@
+package com.cristianml.TomeVault.mapper;
+
+import com.cristianml.TomeVault.dto.response.WishlistBookResponseDTO;
+import com.cristianml.TomeVault.entity.WishlistBookEntity;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class WishlistBookMapper {
+
+    private final ModelMapper modelMapper;
+
+    public WishlistBookResponseDTO toResponseDTO(WishlistBookEntity wishlistBookEntity) {
+        return modelMapper.map(wishlistBookEntity, WishlistBookResponseDTO.class);
+    }
+
+    public List<WishlistBookResponseDTO> wishlistBookResponseDTOList(List<WishlistBookEntity> wishlistBookEntityList) {
+        return wishlistBookEntityList.stream()
+                .map(this::toResponseDTO)
+                .toList();
+    }
+
+}
