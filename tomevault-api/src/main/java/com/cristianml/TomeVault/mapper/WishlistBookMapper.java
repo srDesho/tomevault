08cce9887b96dc35cfd381d5209d49c943ac9a49
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-04-17 18:52:27 -0400

    Editing Mappers

--- tomevault-api/src/main/java/com/cristianml/TomeVault/mapper/WishlistBookMapper.java
+++ tomevault-api/src/main/java/com/cristianml/TomeVault/mapper/WishlistBookMapper.java
@@ -1,8 +1,10 @@
 package com.cristianml.TomeVault.mapper;
 
+import com.cristianml.TomeVault.dto.google.GoogleBookItem;
 import com.cristianml.TomeVault.dto.request.WishlistBookRequestDTO;
 import com.cristianml.TomeVault.dto.response.WishlistBookResponseDTO;
 import com.cristianml.TomeVault.entity.WishlistBookEntity;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -14,6 +16,25 @@ import java.util.List;
 public class WishlistBookMapper {
 
     private final ModelMapper modelMapper;
+
+    @PostConstruct
+    public void configureMappings() {
+        // GoogleBookItem → WishlistEntity
+        modelMapper.typeMap(GoogleBookItem.class, WishlistBookEntity.class)
+                .addMappings(mapper -> {
+                    mapper.map(GoogleBookItem::getId, WishlistBookEntity::setGoogleBookId);
+                    mapper.map(src -> src.getVolumeInfo().getTitle(), WishlistBookEntity::setTitle);
+                    mapper.map(src -> String.join(", ", src.getVolumeInfo().getAuthors()), WishlistBookEntity::setAuthor);
+                    mapper.map(src -> src.getVolumeInfo().getDescription(), WishlistBookEntity::setDescription);
+                    mapper.map(src -> src.getVolumeInfo().getImageLinks() != null ?
+                            src.getVolumeInfo().getImageLinks().getThumbnail() : null, WishlistBookEntity::setThumbnail);
+                    mapper.map(src -> src.getVolumeInfo().getCategories(), WishlistBookEntity::setTags);
+                    mapper.skip(WishlistBookEntity::setId);
+                    mapper.skip(WishlistBookEntity::setUser);
+                    mapper.skip(WishlistBookEntity::setAddedAt);
+
+                });
+    }
 
     public WishlistBookEntity toEntity(WishlistBookRequestDTO wishlistBookRequestDTO) {
         return modelMapper.map(wishlistBookRequestDTO, WishlistBookEntity.class);
