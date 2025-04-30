7fe0b0dbe90adb4f592286cbc95b12eef5c3c81b
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-04-30 02:06:02 -0400

    Add HomePage and install react-router-dom

--- tomevault-web/src/App.jsx
+++ tomevault-web/src/App.jsx
@@ -1,22 +1,11 @@
-// src/App.jsx
 import React from 'react';
-import './App.css';
+import HomePage from './pages/HomePage';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>¡Hola, React!</h1>
-        <p>Tu aplicación React con Vite y Tailwind CSS está lista.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-900 font-sans">
+      <HomePage />
     </div>
   );
 }
