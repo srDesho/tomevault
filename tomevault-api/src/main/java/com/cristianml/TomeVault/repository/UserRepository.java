5cc7c8c0bfb1d63cf3502e2defcca626bb3ebaf3
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-05-15 02:13:10 -0400

    fix(auth): jwt @AuthenticationPrincipal null pointer exception

--- tomevault-api/src/main/java/com/cristianml/TomeVault/repository/UserRepository.java
+++ tomevault-api/src/main/java/com/cristianml/TomeVault/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.cristianml.TomeVault.repository;
 
 import com.cristianml.TomeVault.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findUserEntityByUsername(String username);
