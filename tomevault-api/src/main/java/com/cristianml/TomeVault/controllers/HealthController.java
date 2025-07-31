183e8662d40b9b7d66046a0823c749d58d1eaa5b
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-07-31 08:13:53 -0400

    feat(api): edit health endpoint with method HEAD

--- tomevault-api/src/main/java/com/cristianml/TomeVault/controllers/HealthController.java
+++ tomevault-api/src/main/java/com/cristianml/TomeVault/controllers/HealthController.java
@@ -1,13 +1,15 @@
 package com.cristianml.TomeVault.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
+    @RequestMapping(value = "/health",
+            method = {RequestMethod.GET, RequestMethod.HEAD})
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
     }
